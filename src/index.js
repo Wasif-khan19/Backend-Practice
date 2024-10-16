7bea70a2fd8eafe91c1aeeb854f8f2fc7209542a
Wasif-khan19 <wk5599426@gmail.com>
2024-10-16 15:20:17 +0500

    edited

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// import mongoose from "mongoose";
+// import { MY_DB } from "./constants";
+// import express from 'express'
+
+// const app = express()
+
+//     (async () => {
+//         try {
+//             await mongoose.connect(`${process.env.MONGO_URI}/${MY_DB}`)
+//             app.on("error", (error) => {
+//                 console.log(`Error: ${error}`)
+//             })
+
+//             app.listen(process.env.PORT, () => {
+//                 console.log(`Server is running on port ${process.env.PORT}`)
+//             })
+//         } catch (error) {
+//             console.log(`Error: ${error}`);
+//         }
+//     })()
